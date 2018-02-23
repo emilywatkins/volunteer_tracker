2c7854487f4976c8abfac640d1cbdc5313ba083a
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-23 11:48:17 -0800

    update project.delete method to delete any assigned volunteers, passes test.

--- lib/project.rb
+++ lib/project.rb
@@ -45,6 +45,7 @@ class Project
 
   def delete
     DB.exec("DELETE FROM projects WHERE id = #{self.id};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{self.id};")
   end
 
   def volunteers
