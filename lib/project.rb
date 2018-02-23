b73db0823fff29d6af8c53cda3040d428dff4d6c
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-23 10:23:17 -0800

    add project.delete method to remove projects from the database. passes test.

--- lib/project.rb
+++ lib/project.rb
@@ -41,4 +41,8 @@ class Project
     @id = self.id
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{self.id};")
+  end
 end
