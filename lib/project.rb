92f2064886103b24906f2beec5d2ceebf5c5e86e
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-23 11:13:46 -0800

    add project.volunteer method to return all volunteers for a given project, passes test.

--- lib/project.rb
+++ lib/project.rb
@@ -46,4 +46,16 @@ class Project
   def delete
     DB.exec("DELETE FROM projects WHERE id = #{self.id};")
   end
+
+  def volunteers
+    project_volunteers = []
+    volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{self.id()};")
+    volunteers.each() do |volunteer|
+      name = volunteer['name']
+      project_id = volunteer['project_id'].to_i
+      id = volunteer['id'].to_i
+      project_volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    project_volunteers
+  end
 end
