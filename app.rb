f722e0113473fc828e389ddb328647dc001671c5
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-23 12:21:59 -0800

    add project.erb, project_edit.erb, add new routing to update project title, passes spec.

--- app.rb
+++ app.rb
@@ -19,3 +19,20 @@ post('/') do
   @projects = Project.all
   erb(:index)
 end
+
+get('/projects/:id') do
+  @project = Project.find(params['id'].to_i)
+  erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params['id'].to_i)
+  erb(:project_edit)
+end
+
+patch('/projects/:id') do
+  title = params['title']
+  @project = Project.find(params['id'].to_i)
+  @project.update({:title => title})
+  erb(:project)
+end
