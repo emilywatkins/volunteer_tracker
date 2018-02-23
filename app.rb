15528fcb54f745498903ea06654ed3d2caad532e
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-23 11:39:24 -0800

    add routing to add and show new projects on homepage, passes spec.

--- app.rb
+++ app.rb
@@ -5,6 +5,17 @@ require("./lib/project")
 require("./lib/volunteer")
 require("pg")
 
-# DB = PG.connect({:dbname => 'volunteer_tracker'}) #connect to NON-TEST db
+DB = PG.connect({:dbname => 'volunteer_tracker'})
 
-DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+get('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/') do
+  title = params['title']
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:index)
+end
